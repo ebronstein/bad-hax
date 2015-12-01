bb757587ed1104948143fcfb93221af63362157c
Janny Z <jarzofjam@users.noreply.github.com>
2015-12-01 00:27:42 -0800

    created poo.py

---
+++ poo.py
@@ -0,0 +1,295 @@
+def print_pipe():
+    print("|", end = "")
+
+def print_newline():
+    print()
+
+def print_plus():
+    print("+", end = "")
+
+def print_underscore():
+    print("_", end = "")
+
+def print_underscore_plus():
+    print("_+", end = "")
+
+def print_plus_underscore():
+    print("_+", end = "")
+
+def print_plus_underscore():
+    print("+_", end = "")
+
+def print_minus():
+    print("-", end = "")
+
+def print_slash():
+    print("/", end = "")
+
+def print_backslash():
+    print("\\", end = "")
+
+def print_whitespace():
+    print(" ", end = "")
+
+def print_at():
+    print("@", end = "")
+
+def print_period():
+    print(".", end = "")
+
+def print_tab():
+    for _ in range(4):
+        print_whitespace()
+
+def print_three_plus():
+    print_plus_underscore()
+    print_plus()
+
+def print_three_underscore():
+    print_underscore()
+    print_plus_underscore()
+
+def print_parentheses_one():
+    print("(", end = "")
+
+def print_parentheses_two():
+    print(")", end = "")
+
+
+
+
+
+
+def poop():
+    # line i
+    for _ in range(5):
+        print_tab()
+    print_whitespace()
+    print_three_plus()
+    print_newline()
+
+    # line ii
+    for _ in range(4):
+        print_tab()
+    for _ in range(2):
+        print_whitespace()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+    # line iii
+    for _ in range(3):
+        print_tab()
+    for _ in range(3):
+        print_whitespace()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+    # line iv
+    for _ in range(3):
+        print_tab()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+    # line v
+    for _ in range(2):
+        print_tab()
+    for _ in range(3):
+        print_whitespace()
+    print_plus_underscore()
+    # eye
+    print_whitespace()
+    print_backslash()
+    print_whitespace()
+    # forehead
+    print_underscore_plus()
+    print_underscore()
+    print_three_plus()
+    print_three_underscore()
+    print_three_plus()
+    # eye
+    print_whitespace()
+    print_slash()
+    print_whitespace()
+    print_three_plus()
+    print_newline()
+
+    # line vi
+    for _ in range(2):
+        print_tab()
+    print_whitespace()
+    print_plus_underscore()
+    print_parentheses_one()
+    for _ in range(2):
+        print_whitespace()
+    print_at()
+    print_backslash()
+    print_whitespace()
+    print_parentheses_two()
+    # forehead
+    print_underscore()
+    print_three_plus()
+    print_three_underscore()
+    print_plus()
+    # eye
+    print_parentheses_one()
+    print_whitespace()
+    print_slash()
+    print_at()
+    for _ in range(2):
+        print_whitespace()
+    print_parentheses_two()
+    # temples
+    print_three_plus()
+    print_newline()
+
+    # line vii
+    for _ in range(2):
+        print_tab()
+    print_three_plus()
+    print_underscore_plus()
+    for _ in range(3):
+        print_whitespace()
+    print_backslash()
+    print_three_underscore()
+    print_three_plus()
+    print_three_underscore()
+    print_plus()
+    print_slash()
+    for _ in range(3):
+        print_whitespace()
+    print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+    # line viii
+    print_tab()
+    for _ in range(2):
+        print_whitespace()
+    for _ in range(5):
+        print_three_plus()
+        print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+    # line ix
+    print_tab()
+    for _ in range(2):
+        print_three_plus()
+        print_three_underscore()
+    print_backslash()
+    print_whitespace()
+    print("VVVVVVVV", end = "")
+    print_whitespace()
+    print_slash()
+    for _ in range(2):
+        print_three_plus()
+        print_three_underscore()
+    print_plus()
+    print_newline()
+
+    # line x
+    for _ in range(2):
+        print_whitespace()
+    for _ in range(2):
+        print_three_plus()
+        print_three_underscore()
+    print_three_plus()
+    print_backslash()
+    for _ in range(8):
+        print_underscore()
+    print_slash()
+    print_underscore
+    for _ in range(2):
+        print_three_plus()
+        print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+
+    # line xi
+    print_underscore()
+    for _ in range(7):
+        print_three_plus()
+        print_three_underscore()
+    print_plus_underscore()
+    print_newline()
+
+    # line xii
+    for _ in range(7):
+        print_three_plus()
+        print_three_underscore()
+    print_three_plus()
+    print_newline()
+
+    # line xiii
+    for _ in range(21):
+        print_whitespace()
+        print_period()
+    print_newline()
+
+
+
+def end():
+    # dots
+    for _ in range(21):
+        print_whitespace()
+        print_period()
+    print_newline()
+    # one
+    for _ in range(7):
+        print_three_plus()
+        print_three_underscore()
+    print_three_plus()
+    print_newline()
+    # two
+    print_underscore()
+    for _ in range(7):
+        print_three_plus()
+        print_three_underscore()
+    print_plus_underscore()
+    print_newline()
+
+"""
+
+                     +_+
+                  +_+_+_+_+
+               +_+_+_+_+_+_+_+
+            +_+_+_+_+_+_+_+_+_+_+
+           +_ \ _+_+_+_+_+_+ / +_+
+         +_(  @\ )_+_+_+_+( /@  )+_+
+        +_+_+   \_+_+_+_+_+/   _+_+_+
+      +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
+    +_+_+_+_+_+_\ VVVVVVVV /+_+_+_+_+_+_+
+  +_+_+_+_+_+_+_+\________/_+_+_+_+_+_+_+_+
+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_
++_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
+ . . . . . . . . . . . . . . . . . . . . .
+
+    |-|-|-|-|      |+|+|+|     |/|/|/|/|
+    |-|    |-|   |+|     |+|   |/|     |/|
+    |-|-|-|-|    |+|+|+|+|+|   |/|      |/|
+    |-|    |-|   |+|     |+|   |/|     |/|
+    |-|-|-|-|    |+|     |+|   |/|/|/|/|
+
+    |/|    |\|      /-/\-\      \--\  /--/
+    |/|    |\|     /-/  \-\      \--\/--/
+    |/||||||\|    /-/====\-\      [----]
+    |/|    |\|   /-/      \-\    /--/\--\
+    |/|    |\|  /-/        \-\  /--/  \--\
+
+ . . . . . . . . . . . . . . . . . . . . .
++_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_
+"""
